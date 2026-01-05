b5baa41a6aff6a7d429b7bed654bedbf63293a4a
Ayush501Garg <ayush501bmt@gmail.com>
2026-01-05 10:54:43 +0530

    today changes

--- controllers/batteryController.js
+++ controllers/batteryController.js
@@ -1,14 +1,56 @@
+
+/* CREATE */
 const Battery = require("../models/Battery");
 
 /* CREATE */
 exports.createBattery = async (req, res) => {
   try {
+    if (req.files?.image) {
+      req.body.image_url = `/uploads/${req.files.image[0].originalname}`;
+    }
+
+    if (req.files?.thumbnails) {
+      req.body.thumbnails = req.files.thumbnails.map(
+        file => `/uploads/${file.originalname}`
+      );
+    }
+
     const battery = await Battery.create(req.body);
+
     res.status(201).json({ success: true, data: battery });
   } catch (err) {
     res.status(400).json({ success: false, error: err.message });
   }
 };
+
+/* UPDATE */
+exports.updateBattery = async (req, res) => {
+  try {
+    if (req.files?.image) {
+      req.body.image_url = `/uploads/${req.files.image[0].originalname}`;
+    }
+
+    if (req.files?.thumbnails) {
+      req.body.thumbnails = req.files.thumbnails.map(
+        file => `/uploads/${file.originalname}`
+      );
+    }
+
+    const battery = await Battery.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!battery)
+      return res.status(404).json({ message: "Battery not found" });
+
+    res.json({ success: true, data: battery });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
+};
+
 
 /* GET ALL */
 exports.getAllBatteries = async (req, res) => {
@@ -38,23 +80,7 @@ exports.getBatteryById = async (req, res) => {
   }
 };
 
-/* UPDATE */
-exports.updateBattery = async (req, res) => {
-  try {
-    const battery = await Battery.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true, runValidators: true }
-    );
 
-    if (!battery)
-      return res.status(404).json({ message: "Battery not found" });
-
-    res.json({ success: true, data: battery });
-  } catch (err) {
-    res.status(400).json({ success: false, error: err.message });
-  }
-};
 
 /* DELETE */
 exports.deleteBattery = async (req, res) => {
--- routes/batteryRoutes.js
+++ routes/batteryRoutes.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/batteryController");
+const { upload, handleUploadErrors } = require("../utils/upload");
 
-router.post("/create", controller.createBattery);
+router.post(
+  "/create",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "thumbnails", maxCount: 5 }
+  ]),
+  handleUploadErrors,
+  controller.createBattery
+);
+
+router.put(
+  "/:id",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "thumbnails", maxCount: 5 }
+  ]),
+  handleUploadErrors,
+  controller.updateBattery
+);
+
 router.get("/all", controller.getAllBatteries);
 router.get("/:id", controller.getBatteryById);
-router.put("/:id", controller.updateBattery);
 router.delete("/:id", controller.deleteBattery);
 
 module.exports = router;
